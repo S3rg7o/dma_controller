c6b2c873c8d40930d847930f444c3737100a56c5
S3rg7o <seminara.sergio7@gmail.com>
2018-11-14 18:07:54 +0100

    Added dma verilog files to the cmake installation list

--- rtl/dma_controller.v
+++ rtl/dma_controller.v
@@ -3,7 +3,7 @@ module dma_controller (
 	reset,
 	// Inputs from Device
 	num_words,
-	dev_addr,
+	start_addr,
 	rd_wr,
 	rqst,
 	dev_ack,
@@ -25,22 +25,27 @@ module dma_controller (
 	dma_we
 );
 
-initial begin
-`ifdef SIM $display("DMA: Simulation acquired at %2d",$time) `endif;
-end
+`ifdef SIM
+ initial begin
+  $display("DMA: Simulation acquired at %2d",$time);
+ end
+`endif
 
-parameter ADD_LEN = 8;
-parameter DATA_LEN = 8;
-parameter FIFO_DEPTH = 4; //2^FIFO_DEPTH = regs in the FIFO.
-parameter FIFO_DIV_FACTOR = 3; //by default divide by 8
+parameter ADD_LEN = 16; // Number of bits for the addresses
+parameter DATA_LEN = 16; // Number of bits for the data
+parameter FIFO_DEPTH = 5; // 2^FIFO_DEPTH = regs in the FIFO.
+parameter FIFO_DIV_FACTOR = 3; // by default divide by 8
 
 input clk, reset;
 
 //-------Device interface ------//
-input [FIFO_DEPTH:0] num_words;	//I should be able to write at max. as many words as the fifo can handle (=2^FIFO_DEPTH words),
-								//which you write onto FIFO_DEPTH bits.
-								//Or num_words can bigger and let FIFO_FULL FSM-branch handle the situation.It's up to you.
-input [ADD_LEN-1:0] dev_addr;
+input [ADD_LEN-1:0] num_words;  // 1) I should be able to write at max. as many words as the fifo can handle (=2^FIFO_DEPTH words),
+								// which you write onto FIFO_DEPTH bits.
+								// 2) Or num_words can bigger and let FIFO_FULL FSM-branch handle the situation.It's up to you. +++
+								
+input [ADD_LEN:0] start_addr; // It needs to have one bit more of the address since the device driving the DMA_Controller will address LOGICAL Addresses, therefore it's the DMA that will operate the 1-bit right shift and address the PHYSICAL Mem. Address
+wire [ADD_LEN-1:0] start_addr_shifted = start_addr >> 1;
+
 input rd_wr;
 input rqst;
 input dev_ack;
@@ -59,48 +64,43 @@ output reg dma_en;
 output reg dma_priority; 
 output reg [1:0] dma_we;
 
-//-------- Testbench -------------//
-`ifdef SIM 
-	reg [2**FIFO_DEPTH-1:0] fifo_tb [DATA_LEN-1:0];
-`endif
 
 //--------------------------------//
 //--------------------------------//
 //  Internal variables and wires  //
 //--------------------------------//
 //--------------------------------//
-//Fifo
+// Fifo
 wire fifo_full, fifo_empty, fifo_empty_partial;
 wire [DATA_LEN-1:0] fifo_out, fifo_in;
 reg fifo_rst, fifo_old_add_flag;
 reg fifo_en, fifo_wr_rd;
-//Address register 
+// Address register 
 wire [ADD_LEN-1:0] start_address;
 wire [ADD_LEN-1:0] address;
 reg addr0_rst, addr0_reg_en;
-//Old Address register
+// Old Address register
 wire [ADD_LEN-1:0] old_address;
 reg old_addr_reg_en, old_addr_rst;
-//Flip-flop Mux Add
+// Flip-flop Mux Add
 reg mux;
-//Num_words register
-wire [FIFO_DEPTH:0] words;
+// Num_words register
+wire [ADD_LEN-1:0] words;
 reg words_rst, words_reg_en;
-//Counter
+// Counter
 wire end_count;
-wire [FIFO_DEPTH:0] count;	
-wire [FIFO_DEPTH:0] count_in;	
+wire [ADD_LEN-1:0] count;	
+wire [FIFO_DEPTH-1:0] count_in;	
 reg count_rst, count_en;
-wire count_en_1, count_en_LOAD_STATE;
 reg count_load;
-//FSM control logic  
-reg flag_cnt_words; //end-count for the FSM
-reg msp_or_dev; //1: FIFO out to DMA || 0: FIFO out to DEV
+// FSM control logic  
+wire security_violation;
+reg flag_cnt_words, flag_cnt_words_read; //end-counts for the FSM
+reg out_to_msp; //1: FIFO out to DMA || 0: FIFO out to DEV
 reg error_flag;  
 reg drive_dma_addr; //0: dma_addr = 'hz || 1: dma_addr
 
-
-//FSM States Definition
+// FSM States Definition
 `ifdef SIM
 	reg [15*8:0] state, next_state; //states stored in ASCII 
 `else
@@ -164,10 +164,12 @@ localparam 	IDLE  = 0,
 //--------------------------------//
 //--------------------------------//
 
-//Mux fifo in's and out's
-assign dma_out = msp_or_dev ? fifo_out : {DATA_LEN{1'bz}};
-assign dev_out = msp_or_dev ? {DATA_LEN{1'bz}} : fifo_out;
-assign fifo_in = msp_or_dev ? dev_in : dma_in;
+// Mux fifo in's and out's
+assign dma_out = out_to_msp ? fifo_out : {DATA_LEN{1'bz}};
+assign dev_out = out_to_msp ? {DATA_LEN{1'bz}} : fifo_out;
+assign fifo_in = out_to_msp ? dev_in : dma_in;
+// Check NUM_WORDS and ADDRESS validity 
+assign security_violation = ~|num_words; //if words = 0x0000 then do not even start the count, it will access forever the memory 
 
 fifo #(	.DATA(DATA_LEN), 
 		.ADDR_SIZE(FIFO_DEPTH),
@@ -183,8 +185,8 @@ fifo #(	.DATA(DATA_LEN),
 		.fifo_out(fifo_out),
 		.fifo_old_add_flag(fifo_old_add_flag));
 
-//DMA's internal registers
-register #(.REG_DEPTH(FIFO_DEPTH+1)) word0 (
+// DMA's internal registers
+register #(.REG_DEPTH(ADD_LEN)) word0 (
 				.clk(clk),
 				.reg_en(words_reg_en),
 				.data_in(num_words),
@@ -194,7 +196,7 @@ register #(.REG_DEPTH(FIFO_DEPTH+1)) word0 (
 register #(.REG_DEPTH(ADD_LEN)) addr0 (
 				.clk(clk),
 				.reg_en(addr0_reg_en),
-				.data_in(dev_addr),
+				.data_in(start_addr_shifted),
 				.rst(addr0_rst),
 				.data_out(start_address));
 				
@@ -210,25 +212,22 @@ assign address = start_address + count;
 assign dma_addr = drive_dma_addr ? ( mux ? old_address : address) :
 					{ADD_LEN{1'bz}};
 
-//Counter
-assign count_en_LOAD_STATE = ((state == LOAD_DMA_ADD) & (next_state == READ_MEM)); 
-assign count_en_1 = count_en | count_en_LOAD_STATE;
-//XXX È un po' una suinata ma è la cosa più semplice che ho trovato! Alla fine si trattava di gestire un solo maledetto caso, quindi ho scelto di fare l'hardware ad-hoc. Sono pure due stati consecutivi, quindi hanno solo l'ultimo bit diverso! Vuol dire che solo per l'ultimo bit ho bisogno di 2 AND gate, per gli altri possono essere condivisi.
-
-counter #(.L(FIFO_DEPTH+1)) count0 (
+// Counter
+counter #(.L(ADD_LEN-1)) count0 (
 	.clk(clk),
 	.load(count_load),
 	.rst(count_rst),
-	.cnt_en(count_en_1),
-	.data_in({{FIFO_DEPTH{1'b0}},1'b1}), //necessary for write states, where you need to count 1 time less: the idea is to make the counter start from 1 instead of 0.
+	.cnt_en(count_en),
+	.data_in({{ADD_LEN-2{1'b0}},1'b0}),
 	.cnt(count),
 	.end_cnt(end_count));
 
 always @(count,words) begin
-	flag_cnt_words = (count == words); //NON words-1 PERCHÈ IL CONTEGGIO È FATTO SUL TERZO COLPO DI CLOCK in lettura
+	flag_cnt_words = (count == words-1); 
+	flag_cnt_words_read = (count == words); // flag count for the read case
 end	
 
-//State Assignment
+// State Assignment
 always @(posedge clk,posedge reset)	begin
 	if (reset) begin
 		state <= RESET; //Asynchronus reset	
@@ -236,24 +235,25 @@ always @(posedge clk,posedge reset)	begin
 	end	else state <= next_state;
 end
 
-//Next State Generation
-always @(state, rqst, rd_wr, dma_ready, fifo_full, dma_resp, flag_cnt_words, flag_cnt_words, dev_ack, fifo_empty_partial, reset) begin
-		next_state <= IDLE; //default
+// Next State Generation
+always @(state, rqst, rd_wr, dma_ready, fifo_full, dma_resp, flag_cnt_words, flag_cnt_words_read, dev_ack, fifo_empty_partial, reset) begin
+		next_state <= IDLE; // default
 		case (state)
 			RESET :
 				next_state <= reset ? RESET : IDLE;
 			IDLE : 		
 				next_state <= rqst ? GET_REGS : IDLE;
 			GET_REGS : 
-				next_state <= rd_wr ? LOAD_DMA_ADD : READ_DEV0;
-			//Read 
+				next_state <= rd_wr ? (security_violation ? END_READ : LOAD_DMA_ADD) : 
+							  (security_violation ? END_WRITE : READ_DEV0);
+			// Read 
 			LOAD_DMA_ADD :
 				next_state <= dma_ready ? READ_MEM : LOAD_DMA_ADD;
 			READ_MEM :
-				next_state <= dma_resp? ERROR : 
-							fifo_full ? FIFO_FULL_READ :
-							flag_cnt_words ? SEND_TO_DEV0 :
-							dma_ready ? READ_MEM : OLD_ADDR_RD;
+				next_state <= dma_resp  ? ERROR : 
+							  fifo_full ? FIFO_FULL_READ :
+							  flag_cnt_words_read ? SEND_TO_DEV0 :
+							  dma_ready ? READ_MEM : OLD_ADDR_RD;
 			OLD_ADDR_RD : 
 				next_state <= dma_ready ? READ_MEM : OLD_ADDR_RD;
 			ERROR :
@@ -279,8 +279,8 @@ always @(state, rqst, rd_wr, dma_ready, fifo_full, dma_resp, flag_cnt_words, fla
 				next_state <= SEND_TO_MEM1;
 			SEND_TO_MEM1 :
 				next_state <= dma_resp ? ERROR : 
-							flag_cnt_words ? END_WRITE :
-							dma_ready ? SEND_TO_MEM1 : OLD_ADDR_WR;
+							  flag_cnt_words ? END_WRITE :
+							  dma_ready ? SEND_TO_MEM1 : OLD_ADDR_WR;
 			OLD_ADDR_WR :
 				next_state <= dma_ready ? SEND_TO_MEM1 : OLD_ADDR_WR ;
 			END_WRITE : 
@@ -289,14 +289,14 @@ always @(state, rqst, rd_wr, dma_ready, fifo_full, dma_resp, flag_cnt_words, fla
 			FIFO_FULL_READ : 
 				next_state <= dev_ack ? EMPTY_FIFO_READ : FIFO_FULL_READ;
 			EMPTY_FIFO_READ :
-			next_state <= fifo_empty_partial ? READ_MEM : 
-							dev_ack ? EMPTY_FIFO_READ : FIFO_FULL_READ;
+			    next_state <= fifo_empty_partial ? READ_MEM : 
+						      dev_ack ? EMPTY_FIFO_READ : FIFO_FULL_READ;
 		endcase
 end
 
-//Control Signals Generation
-always @(state) begin
-	//default
+// Control Signals Generation
+always @(state,dma_ready) begin
+	// default
 	addr0_reg_en <= 1'b0;
 	addr0_rst <= 1'b0;	
 	count_en <= 1'b0;
@@ -304,7 +304,7 @@ always @(state) begin
 	count_rst <= 1'b0;
 	dma_ack <= 1'b0;
 	dma_en <= 1'b0;
-	msp_or_dev <= 1'b0;
+	out_to_msp <= 1'b0;
 	dma_priority <= 1'b0;
 	dma_we  <= 2'b00;
 	drive_dma_addr <= 1'b0;
@@ -332,63 +332,68 @@ always @(state) begin
 		end
 		IDLE : 
 		begin
-			count_rst <= 1'b1;	
-			fifo_rst <= 1'b1;//XXX controlla: secondo me in IDLE ci sta svuotare la FIFO e resettare i WR_ADDR e RD_ADDR?
 			addr0_rst <= 1'b1;
+			count_rst <= 1'b1;	
+			fifo_rst <= 1'b1;//XXX controlla: secondo me in IDLE ci sta svuotare la FIFO e resettare i WR_ADDR e RD_ADDR, no?
 			words_rst <= 1'b1;
 		end
 		GET_REGS : 
 		begin
 			addr0_reg_en <= 1'b1;
 			words_reg_en <= 1'b1;
-			dma_ack <= 1'b1;// signal "rqst aquired" to DEV
+			`ifdef SIM 
+			dma_ack <= 1'b1; // signal "rqst aquired" to DEV
+			`endif
 		end
-		//Read 
+		// Read 
 		LOAD_DMA_ADD :  
 		begin
-			fifo_wr_rd <= 1'b1;
-			dma_en <= 1'b1; //impossible to generate dma_ready without dma_en ( line  #171 di memory_backbone)
+			count_en <= 1'b1 & dma_ready;
+			dma_en <= 1'b1; // needed to generate dma_ready (#171 in memory_backbone)
 			drive_dma_addr <= 1'b1;
+			fifo_wr_rd <= 1'b1;
 		end
 		READ_MEM : 
 		begin
+			count_en <= 1'b1;
 			dma_en <= 1'b1;
 			drive_dma_addr <= 1'b1;
 			fifo_en <= 1'b1;
 			fifo_wr_rd <= 1'b1;
-			count_en <= 1'b1;
 			old_addr_reg_en <= 1'b1;
 		end
 		OLD_ADDR_RD:
-		begin
-			mux <= 1'b1;
-			fifo_old_add_flag <= 1'b1;
-			fifo_wr_rd <= 1'b1;
+		begin			
 			dma_en <= 1'b1;
 			drive_dma_addr <= 1'b1;
+			fifo_old_add_flag <= 1'b1;
+			fifo_wr_rd <= 1'b1;
+			mux <= 1'b1;
 		end
 		ERROR :
 		begin
-			error_flag <= 1'b1;
 			drive_dma_addr <= 1'b1;
+			error_flag <= 1'b1;	
 		end
 		SEND_TO_DEV0 : 
 		begin
 			count_rst <= 1'b1;
-			dma_ack <= 1'b1;
+			`ifdef SIM 
+			dma_ack <= 1'b1; // to signal to DEV that the rqst has been aquired
+			`endif
 		end
 		WAIT_READ :
 		begin
-			//NOP: it's not important to signal the DMA request, since the DMA is the master! Every time dev_ack goes LOW, device will say when it's ready again, then it will re-synch again passing by its synchronization 'START_READING' state. 
+			// NOP: it's not important to signal the DMA request, since the DMA is the master! Every time dev_ack goes LOW, device will say when it's ready again, then it will re-synch again passing by its synchronization 'START_READING' state. 
 			//Furthermore, dma_ack will be used as "data_valid" flag and now the avaiabla data is not valid at all
 		end
 		SEND_TO_DEV1 :
 		begin
+			dma_ack  <= 1'b1;
 			count_en <= 1'b1;
-			fifo_en <= 1'b1;
-			dma_ack <= 1'b1;
+			fifo_en  <= 1'b1;
 		end
-		NOP : ;//dma_ack <= 1'b1;
+		NOP : ;// dma_ack <= 1'b1;
 		END_READ : 
 		begin
 			end_flag <= 1'b1;
@@ -396,37 +401,34 @@ always @(state) begin
 		// Write
 		READ_DEV0 :
 		begin
-			count_en <= 1'b1; //this count_en is here only to 
-			count_load <= 1'b1; //enable the this load!
-			msp_or_dev <= 1'b1;
+			out_to_msp <= 1'b1;
 			fifo_wr_rd <= 1'b1;			
 		end
 		READ_DEV1 :
 		begin
-			msp_or_dev <= 1'b1;
-			fifo_wr_rd <= 1'b1;
-			fifo_en <= 1'b1;
 			dma_ack <= 1'b1;
 			count_en <= 1'b1;
+			fifo_wr_rd <= 1'b1;
+			fifo_en <= 1'b1;
+			out_to_msp <= 1'b1;
 		end
 		WAIT_WRITE :
-		begin
-			msp_or_dev <= 1'b1;
-			//dma_ack <= 1'b1;
+		begin		
 			fifo_wr_rd <= 1'b1;
+			out_to_msp <= 1'b1;
 		end
 		SEND_TO_MEM0 :
 		begin
 			count_rst <= 1'b1;
 			dma_we <= 2'b11;
-			msp_or_dev <= 1'b1;
+			out_to_msp <= 1'b1;
 			drive_dma_addr <= 1'b1;
 		end
 		SEND_TO_MEM1 :
 		begin
 			count_en <= 1'b1;
 			dma_en <= 1'b1;
-			msp_or_dev <= 1'b1;
+			out_to_msp <= 1'b1;
 			dma_we <= 2'b11;
 			drive_dma_addr <= 1'b1;
 			fifo_en <= 1'b1;		
@@ -434,17 +436,17 @@ always @(state) begin
 		end
 		OLD_ADDR_WR :
 		begin
-			mux <= 1'b1; 
 			dma_en <= 1'b1;
-			msp_or_dev <= 1'b1;
 			dma_we <= 2'b11;
 			drive_dma_addr <= 1'b1;
-			fifo_en <= 1'b1; // PROVA
+			fifo_en <= 1'b1;
 			fifo_old_add_flag <= 1'b1;
+			mux <= 1'b1; 
+			out_to_msp <= 1'b1;
 		end
 		END_WRITE : 
 		begin
-			msp_or_dev <= 1'b1; //to correctly write the last data
+			out_to_msp <= 1'b1; //to correctly write the last data
 			drive_dma_addr <= 1'b1;
 			end_flag <= 1'b1;
 		end
--- sim/dma_controller_tb.v
+++ sim/dma_controller_tb.v
@@ -17,7 +17,6 @@ wire [FIFO:0] numWords_out;
 wire devReady;
 reg clk, reset;
 
-
 //Inputs from OpenMSP430
 wire [DATA-1:0] dmaOut;
 wire dmaReady, dmaResp;
@@ -39,25 +38,25 @@ reg rdWr;
 
 initial 
 begin 
-$display("SIM: Testbench started at %2d", $time);
-$dumpfile("gtk_views/dma_controller_tb.vcd");
-$dumpvars(0,dma_controller_tb);
-clk = 0;
-startAddress = 0;
-//numWords = FILL_FIFO-1;
-numWords = 1;
-start_device = 1'b0;
-reset = 1'b0;
-#1 start_msp = 1'b1;
-reset = 1'b1;
-rdWr = 1'b1; //Test read
-start_device = 1'b1;
-#1 reset = 1'b0;
-#4 start_device = 1'b0;
-@(posedge devReady) $display("Finished 1!!!!!!!!!!!!!!!!!!!!");
-rdWr = 1'b0; //Test write
-start_device = 1'b1;
-@(posedge devReady) #7 $finish;
+	$display("SIM: Testbench started at %2d", $time);
+	$dumpfile("gtk_views/dma_controller_tb.vcd");
+	$dumpvars(0,dma_controller_tb);
+	clk = 0;
+	startAddress = 0;
+	//numWords = FILL_FIFO-1;
+	numWords = 1;
+	start_device = 1'b0;
+	reset = 1'b0;
+	#1 start_msp = 1'b1;
+	reset = 1'b1;
+	rdWr = 1'b1; //Test read
+	start_device = 1'b1;
+	#1 reset = 1'b0;
+	#4 start_device = 1'b0;
+	@(posedge devReady) $display("Finished 1!!!!!!!!!!!!!!!!!!!!");
+	rdWr = 1'b0; //Test write
+	start_device = 1'b1;
+	@(posedge devReady) #7 $finish;
 end
 
 //Clock generation
@@ -66,74 +65,73 @@ always #1 clk = ~clk;
 device #(	.ADD(ADD),
 			.DATA(DATA),
 			.WORD(FIFO)) 		device_dut ( 
-				//Inputs from DMA
-				.dma_ack(dmaAck),
-				.dev_in(devIn),
-				.dma_end_flag(endFlag),
-				//Inputs from higer logic
-				.clk(clk), //device is sequential
-				.start(start_device),
-				.in_num_words(numWords),
-				.in_start_address(startAddress),
-				.in_rd_wr(rdWr),
-				.reset(reset),
-				//Outputs to DMA
-				.num_words(numWords_out),
-				.start_address(startAddress_out),
-				.rd_wr(rdWr_out),
-				.rqst(Rqst),
-				.dev_ack(devAck),
-				.dev_out(devOut),
-				//Outputs to higher logic
-				.dev_ready(devReady));
+			//Inputs from DMA
+			.dma_ack(dmaAck),
+			.dev_in(devIn),
+			.dma_end_flag(endFlag),
+			//Inputs from higer logic
+			.clk(clk), //device is sequential
+			.start(start_device),
+			.in_num_words(numWords),
+			.in_start_address(startAddress),
+			.in_rd_wr(rdWr),
+			.reset(reset),
+			//Outputs to DMA
+			.num_words(numWords_out),
+			.start_address(startAddress_out),
+			.rd_wr(rdWr_out),
+			.rqst(Rqst),
+			.dev_ack(devAck),
+			.dev_out(devOut),
+			//Outputs to higher logic
+			.dev_ready(devReady));
 				
 				
 				
 dma_controller #(	.ADD_LEN(ADD),
 					.DATA_LEN(DATA),
 					.FIFO_DEPTH(FIFO),//2^FIFO_DEPTH regs
-					.FIFO_DIV_FACTOR(FIFO_DIV_FACTOR))
-		dut ( 
-		.clk(clk),
-		.reset(reset),
-		// Inputs from Device
-		.rd_wr(rdWr_out),
-		.rqst(Rqst),
-		.dev_ack(devAck),
-		.dev_in(devOut),
-		// Outputs to Device
-		.num_words(numWords_out),
-		.dev_addr(startAddress_out),
-		.dma_ack(dmaAck),
-		.dev_out(devIn),
-		.end_flag(endFlag),
-		// Inputs from OpenMSP430
-		.dma_in(dmaOut),
-		.dma_ready(dmaReady), 
-		.dma_resp(dmaResp),
-		// Outputs to OpenMSP430
-		.dma_addr(dmaAddr),
-		.dma_out(dmaDin),
-		.dma_en(dmaEn),
-		.dma_priority(dmaPriority),
-		.dma_we(dmaWe));
+					.FIFO_DIV_FACTOR(FIFO_DIV_FACTOR))	dut ( 
+			.clk(clk),
+			.reset(reset),
+			// Inputs from Device
+			.rd_wr(rdWr_out),
+			.rqst(Rqst),
+			.dev_ack(devAck),
+			.dev_in(devOut),
+			// Outputs to Device
+			.num_words(numWords_out),
+			.start_addr(startAddress_out),
+			.dma_ack(dmaAck),
+			.dev_out(devIn),
+			.end_flag(endFlag),
+			// Inputs from OpenMSP430
+			.dma_in(dmaOut),
+			.dma_ready(dmaReady), 
+			.dma_resp(dmaResp),
+			// Outputs to OpenMSP430
+			.dma_addr(dmaAddr),
+			.dma_out(dmaDin),
+			.dma_en(dmaEn),
+			.dma_priority(dmaPriority),
+			.dma_we(dmaWe));
 
 
 
 MSP430_sim #(	.ADD(ADD),
 				.DATA(DATA)) 	msp_dut ( 
-				//Outputs to DMA
-				.dma_out(dmaOut),
-				.dma_ready(dmaReady), 
-				.dma_resp(dmaResp),
-				 //Inputs from DMA
-				.dma_addr(dmaAddr),
-				.dma_din(dmaDin),
-				.dma_en(dmaEn),
-				.dma_priority(dmaPriority),
-				.dma_we(dmaWe),
-				.clk(clk),
-				.reset(reset));
+			//Outputs to DMA
+			.dma_out(dmaOut),
+			.dma_ready(dmaReady), 
+			.dma_resp(dmaResp),
+			 //Inputs from DMA
+			.dma_addr(dmaAddr),
+			.dma_din(dmaDin),
+			.dma_en(dmaEn),
+			.dma_priority(dmaPriority),
+			.dma_we(dmaWe),
+			.clk(clk),
+			.reset(reset));
 
 
 endmodule
